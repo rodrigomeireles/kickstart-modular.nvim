d523391853fc3957f3320c1b603264ba9315360f
[rodrigomeireles] <rodrigomeirelesdeoliveira@gmail.com>
2024-12-13 07:58:14 -0300

    Adding support for gdscript

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -176,6 +176,7 @@ return {
       --  - capabilities (table): Override fields in capabilities. Can be used to disable certain LSP features.
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
+      require('lspconfig').gdscript.setup {}
       local servers = {
         -- clangd = {},
         -- gopls = {},
